e817209df5c09c59e493c5399fd05d0998645398
liangjinyong <liangjy881@gmail.com>
2023-05-06 12:12:47 +0900

    arithmetic finished

--- ch03_types_values_variables/3.2_numbers.js
+++ ch03_types_values_variables/3.2_numbers.js
@@ -49,3 +49,55 @@ console.log(`Math.log(10): ${Math.log(10)}`); // Natural logarithm of 10
 console.log(`Math.log(100) / Math.LN10: ${Math.log(100) / Math.LN10}`); // Base 10 logarithm of 100
 console.log(`Math.log(512) / Math.LN2: ${Math.log(512) / Math.LN2}`); // Base 2 logarithm of 512
 console.log(`Math.exp(3): ${Math.exp(3)}`); // Math.E cubed
+
+// ES6 defines more functions on the Math object:
+console.log(`Math.cbrt(27): ${Math.cbrt(27)}`); // => 3: cube root
+console.log(`Math.hypot(3, 4): ${Math.hypot(3, 4)}`); // => 5: square root of sum of squares of all arguments
+console.log(`Math.log10(100): ${Math.log10(100)}`); // => 2: Base-10 logarithm
+console.log(`Math.log2(1024): ${Math.log2(1024)}`); // => 10: Base-2 logarithm
+console.log(`Math.log1p(4): ${Math.log1p(4)}`); // => Natural log of (1+x); accurate for very small x
+console.log(`Math.expm1(4): ${Math.expm1(4)}`); // => Math.exp(x)-1; the inverse of Math.log1p()
+console.log(`Math.sign(4): ${Math.sign(4)}`); // -1, 0, or 1 for arguments <, ==, or > 0
+console.log(`Math.imul(2, 3): ${Math.imul(2, 3)}`); // => 6: optimized multiplication of 32 bit integers
+console.log(`Math.clz32(0xf): ${Math.clz32(0xf)}`); // => 28: number of leading zero bits in a 32-bit integer
+console.log(`Math.trunc(3.9): ${Math.trunc(3.9)}`); // => 3: convert to an integer by truncating fractional part
+console.log(`Math.fround(3.1): ${Math.fround(3.1)}`); // Round to nearest 32-bit float number
+console.log(`Math.sinh(Math.PI): ${Math.sinh(Math.PI)}`); // Hyperbolic sine. Also Math.cosh(), Math.tanh()
+console.log(`Math.asinh(Math.PI): ${Math.asinh(Math.PI)}`); // Hyperbolic arcsine. Also Math.acosh(), Math.atanh()
+
+// Predefined global constants in JavaScript
+console.log(`Infinity: ${Infinity}`); // A positive number too big to represent
+console.log(`Number.POSITIVE_INFINITY: ${Number.POSITIVE_INFINITY}`); // The same value
+console.log(`1 / 0: ${1 / 0}`); // => Infinity
+console.log(`Number.MAX_VALUE * 2: ${Number.MAX_VALUE * 2}`); // => Infinity; overflow
+
+console.log(`-Infinity: ${-Infinity}`); // A negative number too big to represent
+console.log(`Number.NEGATIVE_INFINITY: ${Number.NEGATIVE_INFINITY}`); // The same value
+console.log(`-1 / 0: ${-1 / 0}`); // => -Infinity
+console.log(`-Number.MAX_VALUE * 2: ${-Number.MAX_VALUE * 2}`); // => -Infinity
+
+console.log(`NaN: ${NaN}`); // The not-a-number value
+console.log(`Number.NaN: ${Number.NaN}`); // The same value, written another way
+console.log(`0 / 0: ${0 / 0}`); // NaN
+console.log(`Infinity / Infinity: ${Infinity / Infinity}`); // NaN
+
+console.log(`Number.MIN_VALUE / 2: ${Number.MIN_VALUE / 2}`); // => 0: underflow
+console.log(`-Number.MIN_VALUE / 2: ${-Number.MIN_VALUE / 2}`); // => -0: negative zero
+console.log(`-1 / Infinity: ${-1 / Infinity}`); // => -0: also negative zero
+console.log(`-0: ${-0}`);
+
+// The following Number properties are defined in ES6
+console.log(`Number.parseInt('3.4'): ${Number.parseInt("3.4")}`); // Same as the global parseInt() function
+console.log(`Number.parseInt('3.14'): ${Number.parseInt("3.14")}`); // Same as the global parseFloat() function
+console.log(`Number.isNaN(3.14): ${Number.isNaN(3.14)}`); // Is x the NaN value?
+console.log(`Number.isFinite(3.14): ${Number.isFinite(3.14)}`); // Is x a number and finite?
+console.log(`Number.isInteger(3.14): ${Number.isInteger(3.14)}`); // Is x an integer?
+console.log(`Number.isSafeInteger(1234): ${Number.isSafeInteger(1234)}`); // Is x an integer -(2**53) < x < (2**53)?
+console.log(`Number.MIN_SAFE_INTEGER: ${Number.MIN_SAFE_INTEGER}`); // -(2**53 - 1)
+console.log(`Number.MAX_SAFE_INTEGER: ${Number.MAX_SAFE_INTEGER}`); // 2**53 - 1
+console.log(`Number.EPSILON: ${Number.EPSILON}`); // 2**-52: smallest difference between numbers
+
+let zero = 0; // Regular zero
+let negz = -0; // Negative zero
+console.log(`zero === negz: ${zero === negz}`); // => true: zero and negative zero are equal
+console.log(`1 / zero === 1 / negz: ${1 / zero === 1 / negz}`); // => false: Infinity and -Infinity are not equal
