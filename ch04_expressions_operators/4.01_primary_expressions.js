763ef341e3cac021f941abf21a59f61af79984a2
liangjinyong <liangjy881@gmail.com>
2023-05-22 17:21:39 +0900

    4.1 primary expressions

---
+++ ch04_expressions_operators/4.01_primary_expressions.js
@@ -0,0 +1,17 @@
+// 4.1 Primary Expressions
+
+// Literals
+console.log(`1.23: ${1.23}`); // A number literal
+console.log(`"hello": ${"hello"}`); // A string literal
+console.log(`/pattern/: ${/pattern/}`); // A regular expression literal
+
+// Reserved words
+console.log(`true: ${true}`); // Evaluates to the boolean true value
+console.log(`false: ${false}`); // Evaluates to the boolean false value
+console.log(`null: ${null}`); // Evaluates to the null value
+console.log(`this: ${this}`); // Evaluates to the "current" object
+
+// a reference to a variable
+// i // Evaluates to the value of the variable i.
+// sum // Evaluates to the value of the variable sum.
+// undefined // The value of the "undefined" property of the global object.
