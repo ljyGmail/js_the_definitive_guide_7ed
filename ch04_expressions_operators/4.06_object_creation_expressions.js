0a028756d287aabf28b9c5b31d07273cfcac7fef
ljyGmail <liangjy881@gmail.com>
2024-04-08 22:51:14 +0900

    4.6 object creation expressions

---
+++ ch04_expressions_operators/4.06_object_creation_expressions.js
@@ -0,0 +1,6 @@
+console.log(`new Object(): ${new Object()}`);
+
+// If no arguments are passed to the constructor function,
+// the empty pair of parentheses can be omitted:
+// console.log(`new Object: ${new Object}`);
+console.log(`new Date: ${new Date()}`);
