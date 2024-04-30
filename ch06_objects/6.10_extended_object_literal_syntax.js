6b2111a897ffa533aaa3282be2c20332b2349887
ljyGmail <liangjy881@gmail.com>
2024-04-30 14:24:51 +0900

    6.10.5 shorthand methods

--- ch06_objects/6.10_extended_object_literal_syntax.js
+++ ch06_objects/6.10_extended_object_literal_syntax.js
@@ -65,3 +65,43 @@ console.log(q.x); // => 2: the value 2 overrides the previous value from o
 o = Object.create({ x: 1 }); // o inherits the property x
 p = { ...o };
 console.log(p.x); // => undefined
+console.log("-".repeat(30));
+
+// # 6.10.5 Shorthand Methods
+let square = {
+  area: function () {
+    return this.side * this.side;
+  },
+  side: 10,
+};
+
+console.log(square.area()); // => 100
+
+// In ES6, methods can be shortened:
+square = {
+  area() {
+    return this.side * this.side;
+  },
+  side: 10,
+};
+
+console.log(square.area(10)); // => 100
+
+const METHOD_NAME = "m";
+const symbol = Symbol();
+let weirdMethods = {
+  "method With Spaces"(x) {
+    return x + 1;
+  },
+  [METHOD_NAME](x) {
+    return x + 2;
+  },
+  [symbol](x) {
+    return x + 3;
+  },
+};
+
+console.log(weirdMethods["method With Spaces"](1)); // =>
+console.log(weirdMethods[METHOD_NAME](1)); // => 3
+console.log(weirdMethods[symbol](1)); // => 4
+console.log("-".repeat(30));
